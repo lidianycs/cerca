fbc9ebf2f388868194f22bf9f32862bedd4f3cb7
Franz Zieris <franz.zieris@bth.se>
2026-02-18 09:49:58 +0100

    Add test cases for title extraction

--- src/test/java/com/cerca/utils/ReferenceParserTest.java
+++ src/test/java/com/cerca/utils/ReferenceParserTest.java
@@ -17,23 +17,35 @@ public class ReferenceParserTest {
         assertEquals(expectedAuthors, result.authors);
     }
 
+    @ParameterizedTest
+    @MethodSource("referenceProvider")
+    void testTitleExtraction(String input, String ignored, String expectedTitle) {
+        ReferenceParser.ParsedData result = ReferenceParser.parse(input);
+
+        assertEquals(expectedTitle, result.title);
+    }
+
     static Stream<Arguments> referenceProvider() {
         return Stream.of(
                 Arguments.of(
                         "[80] N. Rytilä, “Addressing end-to-end testing challenges with cypress,” 2025.",
-                        "Rytilä, N."
+                        "Rytilä, N.",
+                        "Addressing end-to-end testing challenges with cypress"
                 ),
                 Arguments.of(
                         "[100] T. Zhang, Y. Liu, J. Gao, L. P. Gao, and J. Cheng, “Deep learning paper,” Ieee Software, vol. 37, no. 4, pp. 67–74, 2020.",
-                        "Zhang, T., Liu, Y., Gao, J., Gao, L. P., Cheng, J."
+                        "Zhang, T., Liu, Y., Gao, J., Gao, L. P., Cheng, J.",
+                        "Deep learning paper"
                 ),
                 Arguments.of(
                         "[50] Martina Yvonne Feilzer. Doing mixed methods research pragmatically: Implications for the rediscovery of pragmatism as a research paradigm. Journal of mixed methods research, 4(1):6–16, 2010.",
-                        "Feilzer, Martina Yvonne"
+                        "Feilzer, Martina Yvonne",
+                        "Doing mixed methods research pragmatically: Implications for the rediscovery of pragmatism as a research paradigm"
                 ),
                 Arguments.of(
                         "[20] B. Yetiştiren, I. Özsoy, M. Ayerdem, and E. Tüzün, “Evaluating the code quality of ai-assisted code generation tools: An empirical study on github copilot, amazon codewhisperer, and chatgpt,” arXiv preprint arXiv:2304.10778, 2023. [Online]. Available: https://arxiv.org/abs/2304.10778",
-                        "Yetiştiren, B., Özsoy, I., Ayerdem, M., Tüzün, E."
+                        "Yetiştiren, B., Özsoy, I., Ayerdem, M., Tüzün, E.",
+                        "Evaluating the code quality of ai-assisted code generation tools: An empirical study on github copilot, amazon codewhisperer, and chatgpt"
                 )
         );
     }
